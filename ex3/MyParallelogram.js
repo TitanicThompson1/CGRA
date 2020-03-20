6081d435840d9500bcdcb0936ccad0da7935530e
deborahlago <deborah.lago@hotmail.com>
2020-03-20 17:26:44 +0000

    TP 3 done :)

--- ex3/MyParallelogram.js
+++ ex3/MyParallelogram.js
@@ -15,34 +15,35 @@ class MyParallelogram extends CGFobject{
             2, 0, 0,            //2
             3, 1, 0,            //3
 
-            0, 0, 0,            //0
-            1, 1, 0,            //1
-            2, 0, 0,            //2
-            3, 1, 0             //3
+            0, 0, 0,            //4
+            1, 1, 0,            //5
+            2, 0, 0,            //6
+            3, 1, 0             //7
         ]
-
         //Counter-clockwise reference of vertices
+        this.indices = [
+            0, 2, 1,
+            2, 3, 1,
+            5, 6, 4,
+            5, 7, 6
+        ]
+        this.normals=[]
+        for(let i=0;i<4;i++)
+            this.normals.push(0,0,1);
+        for(let i=0;i<4;i++)
+            this.normals.push(0,0,-1);
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers()
+    }
+}
+
+ /*
         this.indices = [
             0, 2, 1,
             1, 2, 3,
             0, 1, 2,
             1, 3, 2
         ]
-
-        this.normals=[]
-
-        for(let i=0;i<4;i++)
-            this.normals.push(0,0,1);
-
-        for(let i=0;i<4;i++)
-            this.normals.push(0,0,-1);
-
         
+        */
 
-        this.primitiveType = this.scene.gl.TRIANGLES;
-
-        this.initGLBuffers()
-
-
-    }
-}
