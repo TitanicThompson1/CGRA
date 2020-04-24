a15252fd6c18175fdd3ec4c8c6c2932c6cdb3494
TitanicThompson1 <45363817+TitanicThompson1@users.noreply.github.com>
2020-04-24 16:07:54 +0000

    2.1.3 done

--- Projeto/MyVehicle.js
+++ Projeto/MyVehicle.js
@@ -4,16 +4,17 @@
  * @param scene - Reference to MyScene object
  */
 class MyVehicle extends CGFobject{
-    constructor(scene, ang, speed = 0, x, y, z){
+    constructor(scene){
         super(scene);
-        this.ang = ang
-        this.speed = speed
-        this.x = x
-        this.y = y
-        this.z = z
+        this.ang = 0
+        this.speed = 0
+        this.position = [0, 0, 0]
         this.initBuffers();
     }
     initBuffers(){
+
+        
+        
         this.vertices = [
             -0.5, 0, -0.34,          //0
             0, 0, 0.66,              //1
@@ -41,7 +42,32 @@ class MyVehicle extends CGFobject{
     Crie a função MyVehicle.update() para atualizar a variável de posição em
     função dos valores de orientação e velocidade.
     */
+    display(){
+        this.scene.translate(this.position)
+        this.rotate(0, 1, 0, this.ang)
+    }
+
+    /*
+    Crie os métodos turn(val) e accelerate(val) para alterar o ângulo de orientação,
+    e para aumentar/diminuir o valor da velocidade (em que val podem ser valores
+    positivos ou negativos).
+    */
+    turn(val){
+        this.ang += val
+    }
+
+    accelerate(val){
+        this.speed += val
+    }
+
+    reset() {
+        this.ang = 0
+        this.speed = 0
+        this.position = [0, 0, 0]
+    }
+
     update(){
-        
+        this.directionVector = [Math.sin(this.ang) * this.speed, 0, Math.cos(this.ang) * this.speed]
+        this.position = this.position + this.directionVector
     }
 }
